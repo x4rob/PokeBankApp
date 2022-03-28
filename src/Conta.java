e9a8e0964cffbf33966713ac2b9be6d0a742f2c0
x4rob <robson_s.ferreira@hotmail.com>
2022-03-28 20:27:42 -0300

    dando nomes e funções aos bois

---
+++ src/Conta.java
@@ -0,0 +1,38 @@
+
+
+public abstract class Conta implements IConta {
+    private static int AGENCIA_PADRAO = 1;
+    private static  int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(Conta contaDestino, double valor) {
+
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,6 @@
+public class ContaPoupanca extends Conta {
+
+
+
+
+}
